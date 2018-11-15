21a373a63d09e0ac05dc4be2c57588b4d02817e7
Israel Oluwole <olaleyeisrael@gmail.com>
2018-11-15 04:02:20 +0100

    commit before remote repo and heroku deployment

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
     next(error);
 });
 
-// error handling middleware for usage errors in routes
+// error handling middleware for usage errors in routes || all errors
 app.use((error, req, res, next) => {
     // console.log(err);
     res.status(error.status || 500).json({
