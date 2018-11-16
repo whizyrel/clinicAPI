597a84ff9f89697c1bea2c136e47f82112e45109
Israel Oluwole <olaleyeisrael@gmail.com>
2018-11-16 03:17:25 +0100

    added package lock

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 {
     document.querySelector('.shift').addEventListener('click', () => {
         let noOfEmployees = 5;
-        const shiftAPI = `http://localhost:4000/api/shifts?length=${noOfEmployees}`;
+        const shiftAPI = `/api/shifts?length=${noOfEmployees}`;
 
         fetch(shiftAPI)
-        .then(resp => resp.json().then(json => console.log(json)))
-        .catch(err => console.log(err));
+            .then(resp => resp.json().then(json => console.log(json)))
+            .catch(err => console.log(err));
         console.log('shift!');
     });
 
@@ -13,10 +13,10 @@
         console.log('leave!');
         let noOfEmployees = 5;
         let level = 3;
-        const shifLeaveSchdAPI = `http://localhost:4000/api/leaveSchedules?levelNo=${level}&length=${noOfEmployees}`;
+        const shifLeaveSchdAPI = `/api/leaveSchedules?levelNo=${level}&length=${noOfEmployees}`;
 
         fetch(shifLeaveSchdAPI)
-        .then(resp => resp.json()).then(json => console.log(json))
-        .catch(err => console.log(err));
+            .then(resp => resp.json()).then(json => console.log(json))
+            .catch(err => console.log(err));
     });
 }
